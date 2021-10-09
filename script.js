d7aa20996b0290ea4f4a2ef36f8f3eacaa27976d
Akio.I <85005960+AichiGhub@users.noreply.github.com>
2021-10-09 23:14:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function compute()
     var interest = principal * years * rate /100;
     //var year = new Date().getFullYear()+parseInt(years);
     var year = new Date().getFullYear();
-
+    var amount = principal + interest;
 
     // Output string
     var outstr = "If you deposit " + "<mark>" + principal + "</mark>," + "<br/>" +
                  "at an interest rate of " + "<mark>" + rate + "</mark>." + "<br/>" + 
-                 "You will receive an amount of "  + "<mark>"+ principal + interest + "</mark>," + "<br/>" +
+                 "You will receive an amount of "  + "<mark>"+ amount + "</mark>," + "<br/>" +
                  "in the year "  + "<mark>" + year + "</mark>.";
     if (isNaN(principal)){
         //alert("Enter a positive number");
